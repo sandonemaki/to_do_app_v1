a6b067a22b197a8489e1b5f3bda07f356b4a816c
sandonemaki <kamuramaki@gmail.com>
2021-12-29 20:03:50 +0000

    新規タスクページ作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,10 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all.order(created_at: :desc)
   end
+
+  def new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "tasks/index" => "tasks#index"
+  get "tasks/new" => "tasks#new"
+  post "tasks/create" => "tasks#create"
 end
